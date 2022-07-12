1995c05fcd63dedf784bb4c66ee776bfc67ffde9
baha_kenpachi <kenpachi03@icloud.com>
2022-07-13 00:15:35 +0500

    addedmethod set for function isIsogram

--- testing.js
+++ testing.js
@@ -1,18 +1,18 @@
 // Array.prototype.sum = function () {
-//     let s = 0;
-//     for (let i = 0; i<this.length; i++){
-//         if (isNaN(this[i]) == true){
-//             continue;
-//         } else{
-//             s += Number(this[i]);
-//         }
+//   let s = 0;
+//   for (let i = 0; i < this.length; i++) {
+//     if (isNaN(this[i]) == true) {
+//       continue;
+//     } else {
+//       s += Number(this[i]);
 //     }
-//     return s;
+//   }
+//   return s;
 // };
 
 function isIsogram(str) {
   if (str.length == 0) {
-    return console.log('true');
+    return true;
   } else {
     let Arr = str.toLowerCase().split('').sort();
     console.log(Arr);
@@ -21,8 +21,13 @@ function isIsogram(str) {
       Arr[i] != Arr[i + 1] ? (check1 = check1 + 0) : (check1 = check1 + 1);
     }
     console.log(check1);
-    check1 < 1 ? console.log('true') : console.log('false');
+    return check1 < 1 ? true : false;
   }
 }
 
 isIsogram('Obo');
+
+//new  method
+// function isIsogram(str) {
+//   return new Set(str.toUpperCase()).size == str.length;
+// }
